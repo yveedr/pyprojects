1f370900a96148def4e9905b8b76efff3ea658f2
tuoni98 <tuoni98@gmail.com>
2025-07-28 00:46:12 -0300

    nested loop exercise

---
+++ nestedLoop.py
@@ -0,0 +1,11 @@
+#nested loop exercise
+
+rows = int(input("Enter the number of rows: "))
+columns = int(input("Enter the number of columns: "))
+symbol = input("Enter a symbol: ")
+
+for x in range(rows):
+    for y in range(columns):
+        print(symbol, end="")
+    print()
+    
